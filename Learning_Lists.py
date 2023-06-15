8da7bd42ac3c678241e1fb48db847d4f5214d498
AlisherZaitov <133585709+AlisherZaitov@users.noreply.github.com>
2023-06-15 12:34:41 +0500

    Add files via upload

---
+++ Learning_Lists.py
@@ -0,0 +1,62 @@
+#str = "Hallo an Alle"
+#print(list(str))
+
+#str.split(" ") ne rabotayet
+
+#-----------------------------------
+
+
+#liste = [1,2,1,3,1,5,77,89,0]
+
+#print(liste[-6])
+#3
+#-----------------------------------
+
+#listeX = [7,2,14,35,1,0]
+
+#liste = [1,2,1,3,1,5,77,89,0]
+
+
+
+#print(liste[0:5]+listeX[0:3])
+
+#-----------------------------------
+
+#liste = [1,2,1,3,1,5,77,89,0]
+
+#change = input("Would you like to change your number?")
+
+#if change == ("True"):
+#    liste[0] = 6
+#    print("True")
+#elif change == ("False"):
+#    liste[0] = 2
+#    print("False")
+
+#print(liste)
+#-----------------------------------    
+
+#liste = [1,2,1,3,1,5,77,89,0]
+#liste.append(666)
+#liste.append(int(input()))
+#liste.pop()
+#liste.pop(int(input())
+
+
+#print(liste)
+#-----------------------------------
+
+#del(liste[1])
+
+
+#-----------------------------------
+
+#herrn = ['Schneider','Krusspe','Lindemann'] 
+
+#-----------------------------------
+
+arr = [2,3,5,6,6]
+
+arr.remove(max())
+
+print(arr)
---
+++ Triangle_Quest.py
@@ -0,0 +1,6 @@
+n = int(input())
+s = []
+for i in range(1,n):
+    i += 1
+    s.append()        
+print(i)
